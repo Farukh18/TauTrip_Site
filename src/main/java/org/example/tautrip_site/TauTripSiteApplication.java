ee3561b6f61f9cf81d7bfe771960b5cd63907f68
Farukh <excommunicad00@yandex.ru>
2024-02-07 03:00:24 +0600

    initial commit

---
+++ src/main/java/org/example/tautrip_site/TauTripSiteApplication.java
@@ -0,0 +1,13 @@
+package org.example.tautrip_site;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TauTripSiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TauTripSiteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/tautrip_site/TauTripSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.tautrip_site;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TauTripSiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
